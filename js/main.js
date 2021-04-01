2a6ec0b487a164583bd7dd427ce8a504e132f5be
Viraj Nirbhavane <vnirbhavane28@gmail.com>
2021-04-01 19:04:31 +0530

    add Mobile menu

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 const menuBtn = document.querySelector(".menu-btn");
+const menuItems = document.querySelector(".menu-items");
 
 // main toggle
 menuBtn.addEventListener('click', () => {
   console.log('click')
   menuBtn.classList.toggle('open');
+  menuItems.classList.toggle('open');
 })
